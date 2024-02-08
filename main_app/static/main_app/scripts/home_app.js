ca9178ce8d545781ce624992ce7c449eeb0f7917
jonathanespana <tempe.espana@gmail.com>
2024-02-08 03:03:19 -0700

    installed and configured slick slider for product slider on homepage

---
+++ main_app/static/main_app/scripts/home_app.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+    $('.product-slick').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+    // using $ here will be safely even jQuery.noConflict() will be enabled
+    
+});
