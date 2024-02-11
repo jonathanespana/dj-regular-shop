9a1b4aed0bf5d67917eb4c96b80a332150a973b9
jonathanespana <tempe.espana@gmail.com>
2024-02-10 21:20:10 -0700

    added more styling to product detail, connected app to postgres db and added models products and product images

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Product, ProductImage
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Product Information', {'fields': ['product_name', 'price', 'product_description', 'product_fabrics']}),
+        ('Product Categories', {'fields': ['category', 'subcategory']}),
+        ('Product Inventory', {'fields': ['xs', 's', 'm', 'l', 'xl', 'xxl', 'fits_all']}),
+    ]
+
+    list_display = ['product_name', 'product_description', 'product_fabrics', 'category', 'subcategory']
+    inlines = [ProductImageInline]
+
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,44 @@
+from django.contrib.postgres.fields import ArrayField
+
 from django.db import models
 
 # Create your models here.
+
+CATEGORIES=(
+    ('G', 'Genderless'),
+    ('M', 'Men'),
+    ('W', 'Women'),
+    ('A', 'Accessories'),
+)
+
+SUBCATEGORIES=(
+    ('T', 'Tops'),
+    ('B', 'Bottoms'),
+    ('H', 'Headware'),
+    ('A', 'Accessories'),
+)
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, default="Add Product Name")
+    product_description = models.CharField(max_length=500, default="Add Product Description")
+    product_fabrics = models.CharField(max_length=300, default="4way Stretch Recycle Polyester 3L")
+    category = models.CharField(max_length=1, choices=CATEGORIES, default=CATEGORIES[0][0])
+    subcategory = models.CharField(max_length=1, choices=SUBCATEGORIES, default=SUBCATEGORIES[0][0])
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    xs = models.IntegerField(default=0)
+    s = models.IntegerField(default=0)
+    m = models.IntegerField(default=0)
+    l = models.IntegerField(default=0)
+    xl = models.IntegerField(default=0)
+    xxl = models.IntegerField(default=0)
+    fits_all = models.IntegerField(default=0)
+
+    
+    def __str__(self):
+        return self.product_name
+
+
+class ProductImage(models.Model):
+    image_name = models.CharField(max_length= 150)
+    image_url = models.URLField(max_length=200)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
