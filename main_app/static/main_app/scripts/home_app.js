d093e6130cefb07cdd122fa0523c360d54bf3852
jonathanespana <tempe.espana@gmail.com>
2024-03-22 23:33:14 -0600

    responsive css home

--- main_app/static/main_app/scripts/home_app.js
+++ main_app/static/main_app/scripts/home_app.js
@@ -1,8 +1,5 @@
 jQuery(function ($) {
     $('.product-slick').slick({
-        dots: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
     });
     // using $ here will be safely even jQuery.noConflict() will be enabled
     
